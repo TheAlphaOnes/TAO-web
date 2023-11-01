5b0cea0b44248701849885ad3f9e0341e3739ac7
savmg <skbmasale941@gmail.com>
2023-11-01 12:17:03 +0530

    UI modification in HOME

--- src/components/About.js
+++ src/components/About.js
@@ -1,43 +1,85 @@
-import React from 'react'
-import "./About.css"
+import React from "react";
+import "./About.css";
 
 export default function Home() {
   return (
     <>
-    <div>
-  
-  <section id="about"  >
-    <section className="text-white body-font">
-      <div className="container flex flex-wrap px-5 py-24 pb-0 mx-auto items-center">
-        <div className="md:w-2/3 md:pr-12 md:py-5  md:border-b-0  md:mb-0 pb-10  ">
-          <h1 className="sm:text-3xl text-2xl font-medium title-font pb-4  text-white" id="about-title">About Us</h1>
-          <p className="leading-relaxed" id="about-title-dic">TheAlphaOnes: Uniting self-learners and tech enthusiasts, we are a passionate community dedicated to driving innovation, fostering collaboration, and making a meaningful impact through technology.</p>
-        </div>
-      </div></section>
-    <section>
-      <div id="intro-box-con" className='con-about'>
-        <div className="flex flex-col rounded-lg  p-4 pt-6 mybox mybox1" id="intro-box-1">
-          <div className="mb-12">
-            <h1 className="title-about-num">Welcome to TheAlphaOnes!</h1>
-            <p className="about-title-dit">Hello! We are TheAlphaOnes, a dynamic group of students with a passion for all things tech-related. From software development to AI/ML, UI/UX design, and hardware tech, we're constantly exploring the latest innovations and pushing the boundaries of what's possible.</p>
-          </div>
-        </div>
-        <div className="flex flex-col rounded-lg  p-4 pt-6 mybox mybox2" id="intro-box-2">
-          <div className="mb-12">
-            <h1 className="title-about-num">Building a Community to Change the World</h1>
-            <p className="about-title-dit"> We welcome self-learners to join us, as we believe that shared knowledge and expertise can unlock incredible potential. Our team is led by three self-taught developers who share a vision of creating a supportive community that fosters growth, creativity, and collaboration. </p>
-          </div>
-        </div>
-        <div className="flex flex-col rounded-lg  p-4 pt-6 mybox mybox3" id="intro-box-3">
-          <div className="mb-12">
-            <h1 className="title-about-num">Innovation, self-taught and collaboration</h1>
-            <p className="about-title-dit">Our goal is to bring together <br /> like-minded individuals worldwide, pooling our skills and resources  to develop impactful, innovative solutions. Harnessing technology's transformative potential, Driven to make a lasting impact. Join us on our journey to become leading tech giants..</p>
-          </div>
-        </div>
+      <div>
+        <section id="about">
+          <section className="text-white body-font">
+            <div className="container flex flex-wrap px-5 py-24 pb-0 mx-auto items-center">
+              <div className="md:w-2/3 md:pr-12 md:py-5  md:border-b-0  md:mb-0 pb-10  ">
+                <h1
+                  className="sm:text-3xl text-2xl font-medium title-font pb-4  text-white"
+                  id="about-title"
+                >
+                  About Us
+                </h1>
+                <p className="leading-relaxed" id="about-title-dic">
+                  TheAlphaOnes: Uniting self-learners and tech enthusiasts, we
+                  are a passionate community dedicated to driving innovation,
+                  fostering collaboration, and making a meaningful impact
+                  through technology.
+                </p>
+              </div>
+            </div>
+          </section>
+          <section>
+            <div id="intro-box-con" className="con-about">
+              <div
+                className="flex flex-col rounded-lg  p-4 pt-6 mybox mybox1"
+                id="intro-box-1"
+              >
+                <div className="mb-12">
+                  <h1 className="title-about-num">Welcome to TheAlphaOnes!</h1>
+                  <p className="about-title-dit">
+                    Hello! We are TheAlphaOnes, a dynamic group of students with
+                    a passion for all things tech-related. From software
+                    development to AI/ML, UI/UX design, and hardware tech, we're
+                    constantly exploring the latest innovations and pushing the
+                    boundaries of what's possible.
+                  </p>
+                </div>
+              </div>
+              <div
+                className="flex flex-col rounded-lg  p-4 pt-6 mybox mybox2"
+                id="intro-box-2"
+              >
+                <div className="mb-12">
+                  <h1 className="title-about-num">
+                    Building a Community to Change the World
+                  </h1>
+                  <p className="about-title-dit">
+                    {" "}
+                    We welcome self-learners to join us, as we believe that
+                    shared knowledge and expertise can unlock incredible
+                    potential. Our team is led by three self-taught developers
+                    who share a vision of creating a supportive community that
+                    fosters growth, creativity, and collaboration.{" "}
+                  </p>
+                </div>
+              </div>
+              <div
+                className="flex flex-col rounded-lg  p-4 pt-6 mybox mybox3"
+                id="intro-box-3"
+              >
+                <div className="mb-12">
+                  <h1 className="title-about-num">
+                    Innovation, self-taught and collaboration
+                  </h1>
+                  <p className="about-title-dit">
+                    Our goal is to bring together <br /> like-minded individuals
+                    worldwide, pooling our skills and resources to develop
+                    impactful, innovative solutions. Harnessing technology's
+                    transformative potential, Driven to make a lasting impact.
+                    Join us on our journey to become leading tech giants..
+                  </p>
+                </div>
+              </div>
+            </div>
+          </section>
+        </section>
       </div>
-    </section>
-  </section>
-</div>
-
     </>
-  )}
+  );
+}
--- src/components/Launches.js
+++ src/components/Launches.js
@@ -1,49 +1,58 @@
-import React from 'react'
-import Expander from './Expander';
-import "./Launches.css"
+import React from "react";
+import Expander from "./Expander";
+import "./Launches.css";
 
 export default function Launches() {
-
   return (
-<div>
-  <section className="text-white body-font">
-    <div className="container px-3 py-24 mx-auto">
-      <div className="flex flex-wrap w-full mb-15 flex-col items-center text-center">
-        <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 title-gradent">Our Work</h1>
-        <p className="lg:w-1/2 text-xl w-full leading-relaxed ">Discover some of our best launches!!</p>
-      </div>
-      <section className="text-gray-600 body-font">
-
-        {/* every element start  */}
-        
-        <div className="aspect-video container px-5 py-24 mx-auto flex flex-wrap">
-            
-          <div className="lg:w-1/2   w-full mb-10 lg:mb-0 rounded-lg overflow-hidden">
-            <img alt="feature" className="object-cover  object-center  w-full" src="https://dummyimage.com/640x380" />
+    <div>
+      <section className="text-white body-font">
+        <div className="container px-3 py-24 mx-auto">
+          <div className="flex flex-wrap w-full mb-15 flex-col items-center text-center">
+            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 title-gradent">
+              Our Work
+            </h1>
+            <p className="lg:w-1/2 text-xl w-full leading-relaxed ">
+              Discover some of our best launches!!
+            </p>
           </div>
-          <div className="flex flex-col flex-wrap lg:py-6 -mb-10 lg:w-1/2 lg:pl-12 lg:text-left text-center">
-            
-            <h1 className="text-red mynamepr text-2xl mb-2 title-font font-medium mb-3">Name1</h1>
-            
-            <div className="flex flex-col mb-5 lg:items-start items-center">
+          <section className="text-gray-600 body-font">
+            {/* every element start  */}
 
-              <div className="flex-grow">
-                
-                <Expander title="How does the product work?" disc="This is a section of some simple filler text, also known as placeholder text. It shares some characteristics of a real written text but is random or otherwise generated. It may be used to display a sample of fonts or generate text for testing."/>
-                <Expander title="How does the product work?" disc="This is a section of some simple filler text, also known as placeholder text. It shares some characteristics of a real written text but is random or otherwise generated. It may be used to display a sample of fonts or generate text for testing."/>
-                <Expander title="How does the product work?" disc="This is a section of some simple filler text, also known as placeholder text. It shares some characteristics of a real written text but is random or otherwise generated. It may be used to display a sample of fonts or generate text for testing."/>
+            <div className="aspect-video container px-5 py-24 mx-auto flex flex-wrap">
+              <div className="lg:w-1/2   w-full mb-10 lg:mb-0 rounded-lg overflow-hidden">
+                <img
+                  alt="feature"
+                  className="object-cover  object-center  w-full"
+                  src="https://dummyimage.com/640x380"
+                />
+              </div>
+              <div className="flex flex-col flex-wrap lg:py-6 -mb-10 lg:w-1/2 lg:pl-12 lg:text-left text-center">
+                <h1 className="text-red mynamepr text-2xl mb-2 title-font font-medium mb-3">
+                  Name1
+                </h1>
 
+                <div className="flex flex-col mb-5 lg:items-start items-center">
+                  <div className="flex-grow">
+                    <Expander
+                      title="How does the product work?"
+                      disc="This is a section of some simple filler text, also known as placeholder text. It shares some characteristics of a real written text but is random or otherwise generated. It may be used to display a sample of fonts or generate text for testing."
+                    />
+                    <Expander
+                      title="How does the product work?"
+                      disc="This is a section of some simple filler text, also known as placeholder text. It shares some characteristics of a real written text but is random or otherwise generated. It may be used to display a sample of fonts or generate text for testing."
+                    />
+                    <Expander
+                      title="How does the product work?"
+                      disc="This is a section of some simple filler text, also known as placeholder text. It shares some characteristics of a real written text but is random or otherwise generated. It may be used to display a sample of fonts or generate text for testing."
+                    />
+                  </div>
+                </div>
               </div>
             </div>
-
-          </div>
+            {/* every element end  */}
+          </section>
         </div>
-        {/* every element end  */}
-
       </section>
     </div>
-  </section>
-</div>
-
-  )
+  );
 }
