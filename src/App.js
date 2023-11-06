8aa4e38df4a9f7b5ca47f0e1de07f6399b22ab1d
savmg <skbmasale941@gmail.com>
2023-11-06 10:21:39 +0530

    Added Tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,34 +1,26 @@
+import { Route, Routes } from "react-router-dom";
 
-import {
-  Route,
-  
-  Routes
-} from "react-router-dom";
+import IndexPage from "./pages/IndexPage";
+import ContactPage from "./pages/ContactPage";
+import TeamPage from "./pages/TeamPage";
+import ProjectPage from "./pages/ProjectPage";
+import LaunchesPage from "./pages/LaunchesPage";
 
-import IndexPage from "./pages/IndexPage"
-import ContactPage from "./pages/ContactPage"
-import TeamPage from "./pages/TeamPage"
-import ProjectPage from "./pages/ProjectPage"
-import LaunchesPage from "./pages/LaunchesPage"
-
-import './App.css';
-
+import "./App.css";
 
 function App() {
   return (
     <>
-    
-    {/* <BrowserRouter> */}
-    <Routes>
-      <Route path="/" element={ <IndexPage/> } />
-      <Route path="/launches" element={ <LaunchesPage/> } />
-      <Route path="/team" element={<TeamPage/>} />
-      <Route path="/contact" element={<ContactPage/>} />
-      <Route path="/project" element={<ProjectPage/>} />
-    </Routes>
-    
+      {/* <BrowserRouter> */}
+      <Routes>
+        <Route path="/" element={<IndexPage />} />
+        <Route path="/launches" element={<LaunchesPage />} />
+        <Route path="/team" element={<TeamPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/project" element={<ProjectPage />} />
+      </Routes>
 
-    {/* </BrowserRouter> */}
+      {/* </BrowserRouter> */}
     </>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,7 @@ export default function Home() {
             id="tw"
             className="mb-2 text-center text-2xl font-bold  md:mb-2 lg:text-3xl"
           >
-            We Are TheAlphaOnes
+            WE ARE THE ALPHAONES
           </h1>
           <p
             id="disc"
