ea451ae9cda1e63db683ed57d334e96b346d4773
savmg <skbmasale941@gmail.com>
2023-11-07 18:50:52 +0530

    add: Gilroy

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    fontFamily: {
+      gilroy: ["Gilroy", "sans-serif"],
+    },
+    fontWeight: {
+      light: 300,
+      normal: 400,
+      medium: 600,
+      bold: 800,
+      extra: 900,
+    },
+    extend: {},
+  },
+  plugins: [],
+};
