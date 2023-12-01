3c3a51fe64d9c1cf597ca08e7459c10db7a4e2a4
savmg <skbmasale941@gmail.com>
2023-12-01 10:40:35 +0530

    new image added

--- src/components/commons/logos/Logo.jsx
+++ src/components/commons/logos/Logo.jsx
@@ -5,9 +5,9 @@ function Logo() {
       className="flex font-gilroy font-bold-800 order-first min-w-fit lg:order-none lg:w-1/5 title-font font-medium items-center  lg:items-center lg:justify-center mb-4 md:mb-0"
     >
       <img
-        src="https://s3-alpha-sig.figma.com/img/da59/a666/3bad8efbbb2b9583213475cd4a5f8813?Expires=1700438400&Signature=baTrrFSy03YZUfyZaM0vWAgBXt-WDkUoUIx-m4KDuPwd7L4kzSvmJHwG2ZscMvI32H1FHtTsOZjx6ILImoled8rhUu-4lIbLQATGz4uj5Wau-zl9T38K9GZvJIXyIgAFuKvhVD2VFk8DiF0~m2ob8tHKzBTToAW9yTfAmjVlxLZtzu4Qg~2csDBfAAjUujcku2VJ2lku6CgwudAC2oWFlZLrYddLD5Rn-NQoKe9RBlfxRUxWnaoVojoC2vLi52C0QOhxU8hHXsr7t48ffKhl7EqOQi~Vw5NF6UIMV2eYjCjnQK0Mz9c4kFY9gJ5DOLPkuveDQH2H7rfMgJg9-s0DcA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+        src="../../../public/img/theAlphaOnes.png"
         className="w-7 h-7"
-        alt=""
+        alt="o"
       />
       <span className="ml-[11px] text-lg text-white">TheAlphaOnes</span>
     </a>
