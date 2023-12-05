2a064de3bcd686f824d0afa40a5de0dfd11d92da
TheAlphaOnes <115946330+TheAlphaOnes@users.noreply.github.com>
2023-12-05 12:26:30 +0530

    Update Footer.jsx

--- src/components/commons/footer/Footer.jsx
+++ src/components/commons/footer/Footer.jsx
@@ -41,7 +41,7 @@ const credits = [
     link: "https://www.instagram.com/caprisunpapy/",
     name: "Doumbia Al-Hassan aka Caprisunpapy - UI/UX Designer",
   },
-  { link: "https://mrpixel.netlify.app/", name: "Akshat Khare aka BitPixel - Tester &amp; Assister" },
+  { link: "https://mrpixel.netlify.app/", name: "Akshat Khare aka Mr.Pixel - Tester & Assister" },
   {
     link: "https://thecognito.vercel.app",
     name: "Cognito aka Harsh - Developer",
