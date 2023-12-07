9b7b82641c6ebd3cf971c30794a1625d5cb8ba71
savmg <skbmasale941@gmail.com>
2023-12-07 17:59:27 +0530

    loader removed

--- src/pages/AppShell.jsx
+++ src/pages/AppShell.jsx
@@ -1,31 +1,14 @@
 import { Analytics } from "@vercel/analytics/react";
-import { useEffect, useState } from "react";
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { DataContext } from "../contextProvider/DataContext";
 
 import Navbar from "../components/commons/navbar/Navbar";
 import Footer from "../components/commons/Footer";
-import InitialLoader from "../components/commons/InitialLoader";
 
 function AppShell() {
-  const navigate = useNavigate();
-  const [isLoading, setIsLoading] = useState(true);
-
-  function handleLoad() {
-    setIsLoading(false);
-  }
-
-  useEffect(
-    function () {
-      window.addEventListener("load", handleLoad);
-      if (window.location.pathname === "/") navigate("/home");
-    },
-    [navigate]
-  );
 
   return (
     <>
-      {isLoading && <InitialLoader />}
       <Navbar />
       <DataContext>
         <Outlet />
