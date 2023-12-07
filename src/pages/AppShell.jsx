fcb679cd8dd110062510a56e6611f930587be36b
savmg <skbmasale941@gmail.com>
2023-12-07 21:40:02 +0530

    bugfix

--- src/pages/AppShell.jsx
+++ src/pages/AppShell.jsx
@@ -1,12 +1,20 @@
 import { Analytics } from "@vercel/analytics/react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import { DataContext } from "../contextProvider/DataContext";
 
 import Navbar from "../components/commons/navbar/Navbar";
 import Footer from "../components/commons/Footer";
+import { useEffect } from "react";
 
 function AppShell() {
+  const navigate = useNavigate();
 
+  useEffect(
+    function () {
+      if (window.location.pathname === "/") navigate("/home");
+    },
+    [navigate]
+  );
   return (
     <>
       <Navbar />
